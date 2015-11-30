6417788acd87b834a04f3b382d667557b4d6c2c0
Alexander Grebennik <aleks.grebennik@gmail.com>
2015-11-30 18:47:17 +0300

    load data associations

--- _plugins/i18n_data.rb
+++ _plugins/i18n_data.rb
@@ -9,29 +9,46 @@ module Jekyll
       @locale = @site.active_lang
       @default_locale = @site.default_lang
 
-      @content = translate_data(@content)
+      @content = assign_associations(translate_data(@content))
     end
 
     private
+
+      # TODO refactor a lot :)
+
       def translate_data(content)
+        map_content(content) do |key, value|
+          if key.end_with?('_localized') && value.is_a?(Hash)
+            [key.gsub('_localized', ''), value[@locale] || value[@default_locale]]
+          end
+        end
+      end
+
+      def assign_associations(content)
+        map_content(content) do |key, value|
+          if key.end_with?('_id') && value.is_a?(Fixnum)
+            k = key.gsub('_id', '')
+            [k, content[k + 's'].detect{|item| item['id'] == value }] # TODO correct pluralize (may be user activesupport)
+          end
+        end
+      end
+
+      def map_content(content, &block)
         case content
         when Hash
           {}.tap do |h|
             content.each do |key, value|
-              case value
-              when Hash
-                if key.end_with?('_localized')
-                  h[key.gsub('_localized', '')] = value[@locale] || value[@default_locale]
-                else
-                  h[key] = translate_data(value)
-                end
+              k, v = yield(key, value)
+
+              if k.nil? & v.nil?
+                h[key] = map_content(value, &block)
               else
-                h[key] = translate_data(value)
+                h[k] = v
               end
             end
           end
         when Array
-          content.map{|value| translate_data(value) }
+          content.map{|value| map_content(value, &block) }
         else
           content
         end
