c84e0f995829b23d62017b50ebd84d808b1681e5
Vlad Chervanev <vchervanev@yandex.ru>
2015-12-09 16:06:09 +0400

    Sample Hash values filter

--- _plugins/data_helpers.rb
+++ _plugins/data_helpers.rb
@@ -38,3 +38,17 @@ def map_content(content, &block)
   end
 end
 
+module Jekyll
+  module ValuesFilter
+    def values(input)
+      case
+        when input.instance_of?(Hash)
+          input.values
+        else
+          input
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::ValuesFilter)
